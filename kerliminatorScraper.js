12773c91e6ec359476c43de4c3cb9b85f5cd97c9
Lance Tschirhart <lociinthesky@gmail.com>
2018-07-30 22:37:12 -0500

    Scrape memoryLeague for match outcomes and grab kerliminator screenshot.

--- kerliminatorScraper.js
+++ kerliminatorScraper.js
@@ -1,24 +1,6 @@
-var casper = require('casper').create({
-	verbose: true,
-	logLevel: 'error',
-	clientScripts: []
-});
-
-var tabList = []
-
-function getTabs() {
-	var nodeList = document.querySelector('menu a');
-	return Array.prototype.map.call(nodeList, function(node) {
-		return node.innerHTML
-	})
-}
-
-casper.start('http://www.katiekermode.com/Memory/Kerliminator.html')
-
-casper.then(function() {
-	tabList = this.evaluate(getTabs);
-});
-
-casper.run(function(){
-	this.echo(tabList.join('\n -')).exit();
-});
+var casper = require('casper').create()
+	.start('http://www.katiekermode.com/Memory/Kerliminator.html')
+	.run(function(){
+		casper.capture('deck2.png')
+		.exit()
+	});
